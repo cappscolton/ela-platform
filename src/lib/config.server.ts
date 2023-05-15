7554ad6f7654ee6a716389c7122eeb275941fd4a
Colton Capps <cjcapps@ksu.edu>
2023-05-14 22:34:14 -0500

    updates

--- src/lib/config.server.ts
+++ src/lib/config.server.ts
@@ -3,13 +3,20 @@ import { z } from "zod";
 // eslint-disable-next-line no-restricted-imports
 import * as environment from "$env/static/private";
 
-export const ServerConfigSchema = z.object({
-  DATABASE_URL: z.string().trim().min(1).url(),
-  DISCORD_CLIENT_ID: z.string().trim().min(1),
-  DISCORD_CLIENT_SECRET: z.string().trim().min(1),
-  AUTH_SECRET: z.string().trim().min(1),
-});
+export const testConfig = {
+  DATABASE_URL: "",
+  DISCORD_CLIENT_ID: "",
+  DISCORD_CLIENT_SECRET: "",
+  AUTH_SECRET: "",
+};
 
-export type ServerConfigSchema = z.infer<typeof ServerConfigSchema>;
+// export const ServerConfigSchema = z.object({
+//   DATABASE_URL: z.string().trim().min(1).url(),
+//   DISCORD_CLIENT_ID: z.string().trim().min(1),
+//   DISCORD_CLIENT_SECRET: z.string().trim().min(1),
+//   AUTH_SECRET: z.string().trim().min(1),
+// });
 
-export const config = ServerConfigSchema.parse(environment);
+// export type ServerConfigSchema = z.infer<typeof ServerConfigSchema>;
+
+export const config = testConfig;
