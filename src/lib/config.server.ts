4c282b3705263c403e1f159a46c3bd48e7b0f3b4
Colton Capps <cjcapps@ksu.edu>
2023-05-14 22:48:23 -0500

    test: sourcing zod variables from actions secrets for workflows

--- src/lib/config.server.ts
+++ src/lib/config.server.ts
@@ -3,20 +3,13 @@ import { z } from "zod";
 // eslint-disable-next-line no-restricted-imports
 import * as environment from "$env/static/private";
 
-export const testConfig = {
-  DATABASE_URL: "",
-  DISCORD_CLIENT_ID: "",
-  DISCORD_CLIENT_SECRET: "",
-  AUTH_SECRET: "",
-};
+export const ServerConfigSchema = z.object({
+  DATABASE_URL: z.string().trim().min(1).url(),
+  DISCORD_CLIENT_ID: z.string().trim().min(1),
+  DISCORD_CLIENT_SECRET: z.string().trim().min(1),
+  AUTH_SECRET: z.string().trim().min(1),
+});
 
-// export const ServerConfigSchema = z.object({
-//   DATABASE_URL: z.string().trim().min(1).url(),
-//   DISCORD_CLIENT_ID: z.string().trim().min(1),
-//   DISCORD_CLIENT_SECRET: z.string().trim().min(1),
-//   AUTH_SECRET: z.string().trim().min(1),
-// });
+export type ServerConfigSchema = z.infer<typeof ServerConfigSchema>;
 
-// export type ServerConfigSchema = z.infer<typeof ServerConfigSchema>;
-
-export const config = testConfig;
+export const config = ServerConfigSchema.parse(environment);
